3a81b27e647d51e7d60e214dc8b6a9af0bb3aeb4
Jenna Pinkham <pinkhamjenna@gmail.com>
2018-06-01 00:11:55 +0000

    added hex value display

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 $(document).ready(function(){
     // converts hex colors to an rgb array
-    function hexArr(hex) {
-        hex = hex.slice(1);
+    // https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
+    function hexToRgb(hex) {
+        hex = hex.replace(/[^A-Za-z0-9\s]/g,'')
         var bigint = parseInt(hex, 16);
         var r = (bigint >> 16) & 255;
         var g = (bigint >> 8) & 255;
         var b = bigint & 255;
         return [r,g,b];
+    }
+    
+    // converts rgb colors to hex #
+    // https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
+    function rgbToHex(r, g, b) {
+        return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
     }
     
     // turns an array into rgb(x,y,z) form
@@ -25,15 +32,26 @@ $(document).ready(function(){
     
     // creates a new blend
     function createBlend(color1, color2){
-        var newBlend = blendColors(hexArr(color1.val()), hexArr(color2.val()));
+        var newBlend = blendColors(hexToRgb(color1.val()), hexToRgb(color2.val()));
         $('#color-return').attr('style', 'background-color: ' + newBlend);
+        var rgb = newBlend.replace(/[^0-9,]/g, "").split(',').map(x => parseInt(x));
+        $('#hex-blend').html(rgbToHex(rgb[0], rgb[1], rgb[2]));
+    }
+    
+    function setFakeInputs(color1, color2){
+        $('#input1-fake').attr('style', 'background-color: ' + color1.val());
+        $('#input2-fake').attr('style', 'background-color: ' + color2.val());
+        $('#hex1').html(color1.val());
+        $('#hex2').html(color2.val());
     }
     
     // sets the initial blend
     createBlend($('#input1'), $('#input2'));
+    setFakeInputs($('#input1'), $('#input2'))
     
     // changes the blend when a new color is selected
     $('input').change(function(){
         createBlend($('#input1'), $('#input2'));
+        setFakeInputs($('#input1'), $('#input2'))
     })
 });
